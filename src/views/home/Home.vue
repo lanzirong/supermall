29092ea81ff0a75f0d40dd4b29b7fb3e769e6db0
lanzirong <1820159497@qq.com>
2021-03-24 23:37:37 +0800

    完善首页

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,43 +2,134 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <ul>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+      <li>*</li>
+    </ul>
   </div>
 </template>
 
 <script>
 
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from 'network/home'
+
 import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+import { getHomeMultidata, getHomeGoods } from 'network/home'
 
 export default {
   name: "Home",
   data(){
     return {
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods: {
+        'pop': { page:0 ,list: [] },
+        'new': { page:0 ,list: [] },
+        'sell': { page:0 ,list: [] }
+      }
     }
   },
   components:{
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl
   },
   created(){
     //1.请求多个数据
-    getHomeMultidata().then(
-      res => {
-        this.banners = res.data.banner.list;
-        console.log(this.banners)
-        this.recommends = res.data.recommend.list;
-      }
-    )
+    this.getHomeMultidata()
+
+    //2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods:{
+    getHomeMultidata(){
+        getHomeMultidata().then(
+        res => {
+          this.banners = res.data.banner.list;
+          console.log(this.banners)
+          this.recommends = res.data.recommend.list;
+        }
+      )
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page + 1;
+        getHomeGoods(type,page).then(
+        res => {
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1
+        }
+      )
+    }
   }
 }
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px
   }
 </style>
